b923fc9913937c0d2a0afc1f7263c198ce7b36e3
Cung Nguyen <cungjava2000@gmail.com>
2013-08-23 15:33:02 +0700

    add pagin

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ var path = require('path')
 
 
 /**
-* create an and init an instance of the Scraper
+* init an instance of the Scraper
 */
 module.exports.init = function(options){
 	return new Scraper(options);
 }
+module.exports.common = common;
+
+
 
 /**
 * Constructor
@@ -22,29 +25,30 @@ module.exports.init = function(options){
 
 function Scraper(options){
 	var defaultOptions = {
-		cc:1, 
-		plain_text: false, 
+		cc:1, 		
 		delay:5, 
-		timeout:60, 
+		timeout:60*000, 
+		proxy: false,
 		proxies:[],
 		proxy_file: path.resolve(__dirname, 'proxy.txt'), 
-		proxy_auth:'',
-		proxy_enabled: false	
+		proxy_auth:'',		
+		cache: false,
+		dir: path.join(__dirname, "../")
 	};	
-	options = options || {}
-	if(options.proxy_file || options.proxy_auth || options.proxies){
+	this.options = options || {}
+	if(this.options.proxy_file || this.options.proxy_auth || this.options.proxies){
 		//auto enabling the proxies feature when caller provides one of these options
-		options.proxy_enabled = true;
+		this.options.proxy = true;
 	}
 	//override default optiosn with custom options
-	this.options = options = common.mergeObjs(options, defaultOptions); 
+	this.options = common.mergeObjs(this.options, defaultOptions); 
 	
-	this.loadProxies();
+	this.__loadProxies();
 	
 	this.request = require('request');
 	var self = this;
 	var worker = function(options, cb){
-		self.doLoad(options.url, options.options).then(function($){			
+		self.__doLoad(options.url, options.options).then(function($){			
 			cb();//indicate to the queue that this request is done				
 			options.handler(null, $); //pass response data to handler
 			
@@ -55,20 +59,16 @@ function Scraper(options){
 	}
 	this.queue = async.queue(worker, options.cc);
 }
-Scraper.prototype.loadProxies = function(){
-	if(!this.options.proxy_file || !fs.existsSync(this.options.proxy_file)) return;
-	//reset proxies
-	this.options.proxies = [];
-	var self = this;	
-	fs.readFileSync(this.options.proxy_file).toString().split("\n")
-	.forEach(function(proxy){
-		self.options.proxies.push(proxy.trim());	
-	})
-	
-}
 
+//*** API ****
 
+//support: load(url), load(options), or load(url, options)
 Scraper.prototype.load = function(url, options){
+	if(typeof url === 'object'){
+		//case: load(options)
+		options = url;
+		url = options.url;
+	}
 	options = options || {}
 	url = url || '';	
 	if(options.form) options.method = options.method || 'POST';		
@@ -85,11 +85,22 @@ Scraper.prototype.load = function(url, options){
 	return deferred.promise;
 }
 
+Scraper.prototype.pagin = function(options){
+	require('./lib/pager').create({
+		sp: this,
+		init: options.init, 
+		loadedHandler: options.loaded, 
+		doneHandler: options.done || function(err){
+			if(err) console.log(err);
+		}
+	}).start();	
+}
+//***-- end of API ***
+
 /**
 * load given url using Mikeal's Request object
 */
-Scraper.prototype.doLoad = function(url, options ){
-	//console.log(url);
+Scraper.prototype.__doLoad = function(url, options ){
 	
 	var deferred = Q.defer();
 	
@@ -97,58 +108,75 @@ Scraper.prototype.doLoad = function(url, options ){
 	options.headers || (options.headers = {"User-Agent" : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2'} );
 	options.strictSSL = false;
 	options.followAllRedirects = true;
-	if(!options.no_proxy && this.options.proxy_enabled){
+	options.timeout = options.timeout || this.options.timeout;
+	if(this.options.proxy && options.proxy !== false){
 		var proxy = this.options.proxies[Math.floor( (Math.random() * this.options.proxies.length) + 1) - 1].trim();
 		if(this.options.proxy_auth){
 			proxy = 'http://' + this.options.proxy_auth + '@' + proxy;
 		}
 		options.proxy = proxy;
 	}
-	
-	
-	try{
-		this.request(options, function(err, res, body) {	
-			var accept_codes = options.accept_codes || [200];
-			if(err || accept_codes.indexOf(res.statusCode) === -1){				
-				deferred.reject(err || new Error("httpcode: " +res.statusCode));				
-			}else{
-				//success
-				if(options.plain_text){
-					//simply return body as text file
-					deferred.resolve(body);					
+	var sp = this;
+	setTimeout(function(){		
+		try{
+			sp.request(options, function(err, res, body) {	
+				var accept_codes = options.accept_codes || [];
+				accept_codes.push(200);
+				
+				if(err || accept_codes.indexOf(res.statusCode) === -1){				
+					deferred.reject(err || new Error("httpcode: " +res.statusCode));				
 				}else{
-					//var cheerio = require('cheerio');
-					var $ = cheerio.load(body);
-					
-					//make cheerio object xpath-able
-					_.extend($, require('./lib/xpath'));
-					
-					//resolve all relative links to absolute
-					$('a').each(function(i, el){
-						var old = $(el).attr('href');
-						if(old && old.length && !old.contains('javascript') && !old.contains('mailto:') && old[0] !== '#'){
-							$(this).attr('href', urlUtil.resolve(url, old))
-						}
+					//success
+					if(options.plain_text){
+						//simply return body as text file
+						deferred.resolve(body);					
+					}else{
+						//var cheerio = require('cheerio');
+						var $ = cheerio.load(body);
 						
-					})
-					$('form').each(function(i, el){
-						var old = $(el).attr('action');
-						if(old && old.length && !old.contains('javascript') && old[0] !== '#'){
-							$(this).attr('action', urlUtil.resolve(url, old))
-						}
+						//make cheerio object xpath-able
+						_.extend($, require('./lib/xpath'));
 						
-					})
-					//if(res.statusCode !== 200) {console.log(res);process.exit();}					
-					
-					deferred.resolve($);					
+						//resolve all relative links to absolute
+						$('a').each(function(i, el){
+							var old = $(el).attr('href');
+							if(old && old.length && !old.contains('javascript') && !old.contains('mailto:') && old[0] !== '#'){
+								$(this).attr('href', urlUtil.resolve(url, old))
+							}
+							
+						})
+						$('form').each(function(i, el){
+							var old = $(el).attr('action');
+							if(old && old.length && !old.contains('javascript') && old[0] !== '#'){
+								$(this).attr('action', urlUtil.resolve(url, old))
+							}
+							
+						})
+						//if(res.statusCode !== 200) {console.log(res);process.exit();}					
+						
+						deferred.resolve($);					
+					}
 				}
-			}
-		});
-	}catch(err){
-		deferred.reject(err);
-	}
+			});
+		}catch(err){
+			deferred.reject(err);
+		}
+	}, this.options.delay)	
 	
 	return deferred.promise;
 	
  }
+ Scraper.prototype.__loadProxies = function(){
+	if(!this.options.proxy_file || !fs.existsSync(this.options.proxy_file)) return;
+	//reset proxies
+	this.options.proxies = [];
+	var self = this;	
+	fs.readFileSync(this.options.proxy_file).toString().split("\n")
+	.forEach(function(proxy){
+		self.options.proxies.push(proxy.trim());	
+	})
+	
+}
+
+ 
  
